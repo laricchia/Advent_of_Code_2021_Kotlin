a41b37f5853367e5381b0224bd59f4bf3d7455a1
robertoAsus <robertox92@gmail.com>
2021-12-02 09:46:56 +0100

    day 01 solution - second part

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,23 @@
+fun firstPart(measurements : List<Int>) {
+    var count = 0
+
+    if (measurements.isNotEmpty()) {
+        var currItem = measurements.first().toInt()
+        measurements.subList(1, measurements.size).map {
+            if (it > currItem) count++
+            currItem = it
+        }
+    }
+
+    println("Increases = $count")
+}
+
+fun secondPart(measurements: List<Int>) {
+    val windows = measurements.windowed(3)
+    val windowsSums = windows.map { it.reduce { acc, i -> acc + i } }
+    firstPart(windowsSums)
+}
+
 fun main() {
 
     /*
@@ -6,17 +26,9 @@ fun main() {
     * */
 
     val input : List<String> = readInput("Day01")
+    val measurements = input.filter { it.isNotEmpty() }.map { it.toInt() }
 
-    var count = 0
-
-    if (input.isNotEmpty()) {
-        var currItem = input.first().toInt()
-        input.subList(1, input.size).filter { it.isNotEmpty() }.map { it.toInt() }.map {
-            if (it > currItem) count++
-            currItem = it
-        }
-    }
-
-    print("Increases = $count")
+    firstPart(measurements)
+    secondPart(measurements)
 
 }
