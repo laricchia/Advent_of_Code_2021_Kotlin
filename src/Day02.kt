a127238f0b1e07a57f13722569e90bf16c0dda15
robertoAsus <robertox92@gmail.com>
2021-12-02 10:15:03 +0100

    day 02 solution - second part

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,8 +1,12 @@
+private const val CMD_FWD = "forward"
+private const val CMD_UP = "up"
+private const val CMD_DWN = "down"
+
 fun firstPart02(commands : List<Pair<String, Int>>) {
 
-    val fwds = commands.filter { it.first == "forward" }
-    val ups = commands.filter { it.first == "up" }
-    val downs = commands.filter { it.first == "down" }
+    val fwds = commands.filter { it.first == CMD_FWD }
+    val ups = commands.filter { it.first == CMD_UP }
+    val downs = commands.filter { it.first == CMD_DWN }
 
     val finalFwd = fwds.sumOf { it.second }
     val finalUp = ups.sumOf { it.second }
@@ -14,7 +18,26 @@ fun firstPart02(commands : List<Pair<String, Int>>) {
 }
 
 fun secondPart02(commands: List<Pair<String, Int>>) {
+    var aim = 0
+    var depth = 0
+    var position = 0
 
+    commands.map {
+        when (it.first) {
+            CMD_FWD -> {
+                position += it.second
+                depth += (it.second * aim)
+            }
+            CMD_UP -> {
+                aim -= it.second
+            }
+            CMD_DWN -> {
+                aim += it.second
+            }
+        }
+    }
+
+    println("${depth * position}")
 }
 
 
@@ -24,4 +47,5 @@ fun main() {
     val commands = input.filter { it.isNotEmpty() }.map { it.split(" ") }.map { it[0] to it[1].toInt() }
 
     firstPart02(commands)
+    secondPart02(commands)
 }
