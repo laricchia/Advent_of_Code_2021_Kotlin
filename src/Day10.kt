d60a5551bcbedbc82411ad0726e910d7909dae7c
robertoAsus <robertox92@gmail.com>
2021-12-10 11:44:11 +0100

    day 10 solution - second part

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,3 +1,6 @@
+import kotlin.math.floor
+import kotlin.text.StringBuilder
+
 val openingChars = setOf('(', '[', '{', '<')
 val closingChars = setOf(')', ']', '}', '>')
 
@@ -37,7 +40,51 @@ fun firstPart10(list : List<String>) {
 }
 
 fun secondPart10(list : List<String>) {
+    val incompleteLines = mutableListOf<Scope>()
+    for (line in list) {
+        val root = Scope('*', false, null)
+        var currentScope = root
+        incompleteLines.add(root)
 
+        lineCheck@ for (it in line.toCharArray()) {
+            if (it in openingChars) {
+                val newScope = Scope(it, false, currentScope)
+                currentScope.child.add(newScope)
+                currentScope = newScope
+            }
+            if (it in closingChars) {
+                if (it == currentScope.openingChar.expectedClosing()) {
+                    currentScope.isClosed = true
+                    if (currentScope.parent != null) {
+                        val nextSibling = currentScope.parent!!.child.filterNot { it.isClosed }.lastOrNull()
+                        currentScope = nextSibling ?: currentScope.parent!!
+                    } else break@lineCheck
+
+                } else {
+                    incompleteLines.removeLast()
+                    break@lineCheck
+                }
+            }
+        }
+    }
+
+    // println(incompleteLines.size)
+
+    val completitionStrings = incompleteLines.map {
+        val builder = StringBuilder()
+        it.close(builder)
+        builder.toString()
+    }
+
+    // println(completitionStrings)
+
+    val scores = completitionStrings.map {
+        it.removeSuffix("*").fold(0L) { acc, c -> (acc * 5) + c.completionScore() }
+    }
+
+    // println(scores.sorted())
+
+    println(scores.sorted()[floor(scores.size / 2.0).toInt()])
 }
 
 fun main() {
@@ -46,7 +93,7 @@ fun main() {
     val list = input.filter { it.isNotBlank() }
 
     firstPart10(list)
-    // secondPart10(list)
+    secondPart10(list)
 }
 
 data class Scope(
@@ -74,4 +121,20 @@ fun Char.errorScore() : Int {
         '>' -> 25137
         else -> 0
     }
+}
+
+fun Char.completionScore() : Long {
+    return when (this) {
+        ')' -> 1
+        ']' -> 2
+        '}' -> 3
+        '>' -> 4
+        else -> 0
+    }
+}
+
+fun Scope.close(stringBuilder: StringBuilder) {
+    val openChildren = child.filterNot { it.isClosed }
+    openChildren.asReversed().map { it.close(stringBuilder) }
+    if (!isClosed) stringBuilder.append(openingChar.expectedClosing())
 }
